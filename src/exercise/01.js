76c71e09b814d4920c713c7265d62a3e84c01348
Chris Anderson <chris.anderson@zywave.com>
2021-11-29 14:20:58 -0500

    splitting

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,47 +1,49 @@
 // Code splitting
 // http://localhost:3000/isolated/exercise/01.js
 
-import * as React from 'react'
-// 💣 remove this import
-import Globe from '../globe'
+import * as React from "react";
 
-// 🐨 use React.lazy to create a Globe component which uses a dynamic import
-// to get the Globe component from the '../globe' module.
+const loadGlobeModule = () => import(/* webpackPrefetch: true */ "../globe");
+const Globe = React.lazy(loadGlobeModule);
+
+const GlobeSuspenseWrapper = ({showGlobe}) => (
+  <React.Suspense fallback={<div>Loading...</div>}>
+    <div style={{width: 400, height: 400}}>{showGlobe ? <Globe /> : null}</div>
+  </React.Suspense>
+);
 
 function App() {
-  const [showGlobe, setShowGlobe] = React.useState(false)
+  const [showGlobe, setShowGlobe] = React.useState(false);
 
-  // 🐨 wrap the code below in a <React.Suspense /> component
-  // with a fallback.
-  // 💰 try putting it in a few different places and observe how that
-  // impacts the user experience.
   return (
     <div
       style={{
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        height: '100%',
-        padding: '2rem',
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: "100%",
+        padding: "2rem",
       }}
     >
-      <label style={{marginBottom: '1rem'}}>
+      <label
+        style={{marginBottom: "1rem"}}
+        onMouseEnter={loadGlobeModule}
+        onFocus={loadGlobeModule}
+      >
         <input
           type="checkbox"
           checked={showGlobe}
           onChange={e => setShowGlobe(e.target.checked)}
         />
-        {' show globe'}
+        {" show globe"}
       </label>
-      <div style={{width: 400, height: 400}}>
-        {showGlobe ? <Globe /> : null}
-      </div>
+      <GlobeSuspenseWrapper showGlobe={showGlobe} />
     </div>
-  )
+  );
 }
 // 🦉 Note that if you're not on the isolated page, then you'll notice that this
 // app actually already has a React.Suspense component higher up in the tree
 // where this component is rendered, so you *could* just rely on that one.
 
-export default App
+export default App;
